e429a18558173ff0cf8d05048a70278ae0b1046e
Subroto Kumar <subrotokumar@outlook.in>
2026-03-24 19:17:29 +0000

    Refactor Helm templates and values, update ArgoCD setup, and enhance taskfile

--- k8s/setup.argocd.sh
+++ k8s/setup.argocd.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# kubectl create namespace argocd
+# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+helm repo add argo https://argoproj.github.io/argo-helm
+helm install argocd argo/argo-cd --namespace argocd --create-namespace
 
 # Optional
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
